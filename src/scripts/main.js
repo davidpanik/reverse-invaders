544bb39313023211965c7cf67f93b2b7f9133b18
David Storey <david.storey@realise.com>
2019-08-16 09:06:58 +0100

    Added test random player

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,75 @@
 import { init, Sprite, GameLoop } from './kontra';
-console.log(init);
 let { canvas } = init();
 
-let sprite = Sprite({
-	x: 100,
-	y: 80,
+let player = new Sprite({
+	x: (canvas.width / 2) - 10,
+	y: canvas.height - 40,
 	color: 'red',
 	width: 20,
-	height: 40,
+	height: 20,
 	dx: 2
 });
 
-let loop = GameLoop({
-	update: function () {
-		sprite.update();
+let playerFire = [];
 
-		if (sprite.x > canvas.width) {
-			sprite.x = -sprite.width;
+let loop = new GameLoop({
+	update: function () {
+		player.update();
+		playerFire.forEach((fire) => fire.update());
+
+		// Bounce on edges
+		if (player.x > canvas.width - player.width || player.x < 0) {
+			player.dx *= -1;
+		}
+
+		if (random(40) === 1) {
+			player.dx *= -1;
+		}
+
+		if (random(30) === 1) {
+			playerFire.push(Sprite({
+				x: player.x + (player.width / 2),
+				y: player.y - 10,
+				color: 'blue',
+				width: 5,
+				height: 10,
+				dy: -5
+			}));
 		}
 	},
 	render: function () {
-		sprite.render();
+		player.render();
+		playerFire.forEach((fire) => fire.render());
 	}
 });
 
 loop.start();
+
+function random(r1, r2, r3) {
+	let min = 0;
+	let max = 0;
+	let interval = 1;
+	let result;
+
+	if (typeof (r1) !== 'undefined') {
+		min = 0;
+		max = r1;
+	}
+
+	if (typeof (r2) !== 'undefined') {
+		min = r1;
+		max = r2;
+	}
+
+	if (typeof (r3) !== 'undefined') {
+		interval = r3;
+	}
+
+	result = Math.floor(Math.random() * (max - min)) + min;
+
+	if (interval > 1) {
+		result = Math.round(result / interval) * interval;
+	}
+
+	return result;
+}
