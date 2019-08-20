ea9d5e85dbcf436398a3915ea29f0fe600c80b2a
David Storey <david.storey@realise.com>
2019-08-20 08:53:51 +0100

    Added weapon cooldowns

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,33 @@
 
 TODO
 	Display player lives
-	Detect when all aliens are dead
 	Partical collision effects
 	Sound effects
 	Work out lowest alien in each column
-	Add cooldown to weapons
 	Indicate which aliens are about to fire
 	Add special alien
 	Bring a dead alien back when special alien crosses the screen
 	Make player aim for specific aliens
 	Make player attempt to dodge bullets
 	Start splitting out code
+	Host on Netlify
+	Increase alien speed as game progresses
+	Handle game end
 
 */
 
 
-import { init, Sprite, GameLoop, Pool, initKeys, keyPressed } from './kontra';
+import { init, Sprite, GameLoop, Pool, initKeys, keyPressed } from './vendor/kontra';
 import { chance, random } from './random';
 
 let { canvas } = init();
 
 initKeys();
 
-const gutter = 20;
+const gutter = 10;
 let playerLives = 5;
-
+let playerCooldown = 1000;
+let alienCooldown = 1000;
 
 let player = new Sprite({
 	x: (canvas.width / 2) - 10,
@@ -35,7 +37,8 @@ let player = new Sprite({
 	width: 30,
 	height: 20,
 	anchor: { x: 0.5, y: 0.5 },
-	dx: 2
+	dx: 2,
+	weaponReady: true
 });
 
 let aliens = [];
@@ -49,7 +52,8 @@ for (let x = 0; x < 4; x++) {
 			width: 40,
 			height: 40,
 			anchor: { x: 0.5, y: 0.5 },
-			alive: true
+			alive: true,
+			weaponReady: true
 		}));
 	}
 }
@@ -84,10 +88,23 @@ function getLowestAliens() {
 	return aliens.filter((alien) => alien.alive).filter((alien) => alien.y === lowestAlien.y);
 }
 
+function getLivingAliens() {
+	return aliens.filter((alien) => alien.alive);
+}
+
+function getDeadAliens() {
+	return aliens.filter((alien) => alien.alive !== false);
+}
+
 let loop = new GameLoop({
 	update: function () {
+		if (getDeadAliens().length <= 0) {
+			alert('GAME OVER');
+			window.location = window.location;
+		}
+
 		player.update();
-		aliens.filter((alien) => alien.alive).forEach((alien) => alien.update());
+		getLivingAliens().forEach((alien) => alien.update());
 		playerMissiles.update();
 		alienMissiles.update();
 
@@ -100,35 +117,49 @@ let loop = new GameLoop({
 		}
 
 		if (chance(50)) {
-			playerMissiles.get({
-				x: player.x + (player.width / 2),
-				y: player.y - (player.height / 2),
-				color: 'green',
-				width: 5,
-				height: 15,
-				anchor: { x: 0.5, y: 0.5 },
-				dy: -3,
-				ttl: canvas.height
-			});
-		}
-
-		if (chance(100)) {
-			getLowestAliens().forEach((alien) => {
-				alienMissiles.get({
-					x: alien.x + (alien.width / 2),
-					y: alien.y + (alien.height / 2),
-					color: 'yellow',
+			if (player.weaponReady) {
+				playerMissiles.get({
+					x: player.x,
+					y: player.y - (player.height / 2),
+					color: 'green',
 					width: 5,
 					height: 15,
 					anchor: { x: 0.5, y: 0.5 },
-					dy: 3,
+					dy: -3,
 					ttl: canvas.height
 				});
-			});
+
+				player.weaponReady = false;
+				setTimeout(() => {
+					player.weaponReady = true;
+				}, playerCooldown);
+			}
 		}
+		
+		getLowestAliens().forEach((alien) => {
+			if (chance(100)) {
+				if (alien.weaponReady) {
+					alienMissiles.get({
+						x: alien.x,
+						y: alien.y + (alien.height / 2),
+						color: 'yellow',
+						width: 5,
+						height: 15,
+						anchor: { x: 0.5, y: 0.5 },
+						dy: 3,
+						ttl: canvas.height
+					});
+
+					alien.weaponReady = false;
+					setTimeout(() => {
+						alien.weaponReady = true;
+					}, alienCooldown);
+				}
+			}
+		});
 
 		playerMissiles.getAliveObjects().forEach((missile) => {
-			aliens.filter((alien) => alien.alive).forEach((alien) => {
+			getLivingAliens().forEach((alien) => {
 				if (missile.collidesWith(alien)) {
 					alien.alive = false;
 					missile.ttl = 0;
@@ -143,7 +174,8 @@ let loop = new GameLoop({
 				missile.ttl = 0;
 
 				if (playerLives <= 0) {
-					console.log('YOU WIN!');
+					alert('YOU WIN!');
+					window.location = window.location;
 				}
 			}
 		});
@@ -168,7 +200,7 @@ let loop = new GameLoop({
 	},
 	render: function () {
 		player.render();
-		aliens.filter((alien) => alien.alive).forEach((alien) => alien.render());
+		getLivingAliens().forEach((alien) => alien.render());
 		playerMissiles.render();
 		alienMissiles.render();
 	}
